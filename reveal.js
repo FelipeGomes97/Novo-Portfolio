51e675475b1537df8552ffce411843748f63480b
Felipe Gomes <152811385+FelipeGomes97@users.noreply.github.com>
2024-07-07 23:41:51 +0100

    Add files via upload

---
+++ reveal.js
@@ -0,0 +1,76 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.foto-sobre',
+{
+    duration: 1000,
+    distance: '150px'
+})
+
+revelar.reveal('.sobre-mim',
+{
+    duration: 1000,
+    distance: '150px',
+    origin: 'right'
+})
+    
+revelar.reveal('.botoes',
+{
+    duration: 1000,
+    distance: '150px',
+    origin: 'left'
+})
+
+revelar.reveal('.soft-skills',
+{
+    duration: 1000,
+    distance: '150px',
+    origin: 'right'
+})
+
+revelar.reveal('.fa-html5',
+{
+    duration: 500,
+    distance: '150px',
+    delay: 500
+})
+
+revelar.reveal('.fa-css3-alt',
+{
+    duration: 500,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.fa-js',
+{
+    duration: 500,
+    distance: '90px',
+    delay: 1500
+})
+
+revelar.reveal('.fa-java',
+{
+    duration: 500,
+    distance: '90px',
+    delay: 2000
+})
+
+revelar.reveal('.projeto-psico',
+{
+    duration: 1000,
+    distance: '150px',
+    origin: 'right'
+})
+
+revelar.reveal('.foto-felipe',
+{
+    duration: 1000,
+    distance: '150px'
+})
+
+revelar.reveal('.contato',
+{
+    duration: 1000,
+    distance: '150px',
+    origin: 'right'
+})
